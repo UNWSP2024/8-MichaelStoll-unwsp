7e8d755d3a29e7566afc0c8da1ec4837ec800c0f
MichaelStoll-unwsp <mastoll@students.unwsp.edu>
2025-03-19 10:01:27 -0500

    Add files via upload

--- program_2.py
+++ program_2.py
@@ -1,23 +1,13 @@
-# Program #2: Word Separator
-# Write a program that accepts as input a sentence in which all of the words are run together, 
-# but the first character of each word is uppercase.  
-# Convert the sentence to a string in which the words are separated by spaces, 
-# and the first word starts with an uppercase.  
-# For example the string "StopAndSmellTheRoses" would be converted to "Stop and smell the roses."
-
-# Start your changes on line 13
-
-def word_separator(sentence):
-
-    new_sentence = ""
-    #    Add your logic here
-
-    return new_sentence.strip()
-
-# Example usage
-
-sentence = "StopAndSmellTheRoses"
-
-new_sentence = word_separator(sentence)
-
-print(new_sentence)
+#title: word separator
+#author: michael stoll
+#date: 3/17/2025
+result = ' '
+squished_words = str(input('Enter sentence without spaces and where each word is capitalized:'))
+result = result + squished_words[0]
+for c in range(1, len(squished_words)):
+    character = squished_words[c]
+    if character.isupper():
+        character = character.lower()
+        result = result + ' '
+    result = result + character
+print(result)
--- program_3.py
+++ program_3.py
@@ -1,8 +1,38 @@
-# Program #3: Capital Quiz
-# Write a program that creates a dictionary containing the U.S. states as keys, 
-# and their capitals as values.  
-# The program should then randomly quiz the user by displaying the name of a state 
-# and asking the user to enter the state's capital.  
-# The program should count of the number of correct and incorrect responses.  
-# (You could alternatively use another country and provinces, 
-# or countries of the world and capitals).
+#title:capitals quiz
+#author: michael stoll
+#date: 3/17/2025
+import random
+score = 0
+play_again = 'true'
+capitals = {'England': 'London', 'France': 'Paris', 'Germany': 'Berlin', 'Spain': 'Madrid',
+            'Italy': 'Rome', 'Greece': 'Athens', 'Albania': 'Tirana', 'Bulgaria': 'Sofia'}
+
+while play_again == 'true':
+    question = random.choice(list(capitals.keys()))
+    print(f'What is the capital of {question}?')
+
+    if question == 'England':
+        correct_answer = 'London'
+    elif question == 'France':
+        correct_answer = 'Paris'
+    elif question == 'Germany':
+        correct_answer = 'Berlin'
+    elif question == 'Spain':
+        correct_answer = 'Madrid'
+    elif question == 'Italy':
+        correct_answer = 'Rome'
+    elif question == 'Greece':
+        correct_answer = 'Athens'
+    elif question == 'Albania':
+        correct_answer = 'Tirana'
+    else:
+        correct_answer = 'Sofia'
+
+    answer = input('Enter answer:')
+    if answer == correct_answer:
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect. The correct answer is", correct_answer)
+    print('Score is', score, 'point(s)')
+    play_again = str(input('Enter \"true\" if you want to play again:'))
